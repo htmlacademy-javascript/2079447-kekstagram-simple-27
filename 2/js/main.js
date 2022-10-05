027e6b469b430d0a3b5d25ca31d48c810c4fafc8
Keks <keks@htmlacademy.ru>
2022-10-05 20:57:01 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,27 +1,12 @@
-//Функция для проверки максимальной длины строки.
-//Ссылка на источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length
-
-function checkStringLength(string, maxLength) {
-  return string.length < maxLength;
-}
+const checkStringLength = (string, maxLength) => string.length < maxLength;
 
 checkStringLength('Короткий комментарий', 20);
-checkStringLength('Длинный комментарий', 140);
 
-// const smallComment ="Длина комментария не должна быть менее 20 символов";
-// const bigComment = "Длина комментария не должна быть менее 140 символов";
-
-// console.log('Текст занимает ' + smallComment.length + ' кодовых значений');
-// console.log('Текст занимает ' + bigComment.length + ' кодовых значений');
-
-//Функция, возвращающая случайное целое число из переданного диапазона включительно.
-//Ссылка на источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
-function getRandomArbitrary(min, max) {
-  if (min < 0 || max < 0) {
+const getRandomArbitrary = (min, max) => {
+  if (min < 0 || max < 0 || min >= max) {
     return NaN;
   }
 
   return (Math.random() * (max - min) + min);
-}
+};
 getRandomArbitrary(1, 10);
