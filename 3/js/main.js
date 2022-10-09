adb96c6b5d8abb8f180e23c6dcf817af55ac995c
Keks <keks@htmlacademy.ru>
2022-10-10 01:06:37 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,71 @@
+const checkStringLength = (string, maxLength) => string.length < maxLength;
+
+checkStringLength('Короткий комментарий', 20);
+
+
+const PHOTOVISUAL = [
+  'love',
+  'people',
+  'animals',
+  'place',
+  'vacation',
+  'food',
+  'man',
+  'girl',
+  'kids',
+  'birds',
+  'islands',
+  'capitals',
+  'faces',
+  'pets',
+  'beach',
+  'sky',
+  'forest',
+  'nature',
+];
+
+const ADJECTIVES = [
+  'adorable',
+  'beautiful',
+  'wonderful',
+  'amazing',
+  'better',
+  'charming',
+  'delicious',
+  'Astonishing',
+  'lovely',
+  'excited',
+  'fancy',
+  'attractive',
+  'excellent',
+  'fabulous',
+  'modern',
+  'pleasant',
+  'alluring',
+  'delightful',
+];
+
+const GENERATE_PHOTOS = 25;
+
+const getRandom = (a, b) => {
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandom(0, elements.length - 1)];
+
+const createPhotos = () => ({
+  id: getRandom(1, 25),
+  url: `photos/${getRandom(1, 25)}.jpg`,
+  description: `${getRandomArrayElement(ADJECTIVES)} ${getRandomArrayElement(PHOTOVISUAL)}`,
+  likes: getRandom(15, 200),
+  comments: getRandom(0, 200),
+});
+
+const differentPhotos = Array.from({ length: GENERATE_PHOTOS }, createPhotos);
+console.log(differentPhotos);
