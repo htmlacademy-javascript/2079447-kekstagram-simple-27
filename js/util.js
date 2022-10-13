ff012a3c1db8661c4e1b82ba8264a07fd916150c
Наталья <bulauka@mail.ru>
2022-10-13 23:01:33 +0400

    Модульность

---
+++ js/util.js
@@ -0,0 +1,20 @@
+const checkStringLength = (string, maxLength) => string.length < maxLength;
+
+checkStringLength('Короткий комментарий', 20);
+
+const getRandom = (a, b) => {
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandom(0, elements.length - 1)];
+
+export {
+  getRandom,
+  getRandomArrayElement,
+};
